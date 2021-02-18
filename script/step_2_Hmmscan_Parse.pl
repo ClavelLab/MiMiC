389d2ccc8cc296a1cdb0871148f018d8e1c163ea
The Clavel lab <74304802+ClavelLab@users.noreply.github.com>
2021-02-18 09:38:37 +0100

    Update step_2_Hmmscan_Parse.pl

--- script/step_2_Hmmscan_Parse.pl
+++ script/step_2_Hmmscan_Parse.pl
@@ -1,26 +1,49 @@
-#!usr/bin/perl 
-#Neeraj kumar
-#script to parse hmmscan output
-#last update 20 mrach 2020
+#Author Neeraj Kumar
+#Last updated 2021
 
-#use : perl step_1_Hmmscan.pl
-#need to mention the input and output directory in the variable $srcdir (input directory), $dest(output directory)
+#Description
+# script parsed the hmmscan out put for step 3 
+
+#Usage
+# perl step_2_hmmscan_Parsed_v2.pl path_to_input_folder*
+# the input folder should contain only faa files inside.
+
+
+#!usr/bin/perl 
 use strict;
 use warnings;
 use File::Copy;
 
-my $srcdir = "/path to input directory";  #source directory
-my $dest ="path to output directory";  #target directory
+my $srcdir = $ARGV[0]; # path for source directory where hmmscan output tables are present.
+
+sub main {
+    my $directory = "hmmscan_parsed";
+    
+    unless(mkdir $directory) {
+        die "Unable to create $directory\n";
+    }
+}
+
+main();
+
+my $dest ="hmmscan_parsed/"; #target directory
+
+#my $srcdir = "/media/sparekh/External_storage/mbarc_analysis_2019/mbarc_2019/table"; #source directory
+#my $dest ="/media/sparekh/External_storage/mbarc_analysis_2019/mbarc_2019/table_Parsed"; #target directory
 
 opendir (DIR, $srcdir) or die "can not open $srcdir,$!";
+#my $file_to_write = 'report.txt';
+#open(my $fh_t, '>', $file_to_write) or die "Could not open file '$file_to_write' $!";
 
-my @files; 
+my @files; #programe does not work if you do not defined variable a local before using it.(very important)
 my @line_array;
 my @column1;
 my ($target_name, $accession_FS, $queryname, $accession,$E_value_FS, $score_FS,$bias_FS,$E_value_BD, $score_BD, $bias_BD, $exp_DNE, $reg_DNE, $clu_DNE, $ov_DNE,
 $env_DNE,$dom_DNE,$rep_DNE,$inc_DNE,$description_of_target)=0;
 
-@files = grep {!/^\.+$/ } readdir(DIR);
+
+
+@files = grep {!/^\.+$/ } readdir(DIR); #grep all filenames in array
 
 foreach my $file (@files)
 	{
@@ -32,12 +55,16 @@ foreach my $file (@files)
 		{ 
 			unless ($line =~ /#/) 
 			{ 
-				
+				#print $line;
+				#@line_array = split('\s+',$line);#''\s+' split the line based on multiple array.
+				#@column1 = ("$line_array[0]\t",$line_array[1]);
 				($target_name ,$accession_FS, $queryname,  $accession,  $E_value_FS, $score_FS, $bias_FS,  $E_value_BD, $score_BD, $bias_BD,  $exp_DNE,$reg_DNE, $clu_DNE, $ov_DNE, $env_DNE, $dom_DNE, $rep_DNE, $inc_DNE, $description_of_target)=split('\s+',$line);
 				unless ( $E_value_FS >= 0.01 )
 					{
 						
 						print $fh_t "$accession_FS\n";
+						#print $fh_t "$queryname\t$accession_FS\t$E_value_FS\n";
+						#print $fh_t "$E_value_FS\n";
 						print "$accession_FS\t";
 						print "$E_value_FS\n";
 					}
@@ -47,9 +74,3 @@ foreach my $file (@files)
 		}
 #		move($old, $dest) or die "Move $old -> $dest failed: $!"; #to move files from one folder to another.
 	}  
-
-
-#script ends here
-
-
-
